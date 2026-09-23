41ba2c0ba5fa9572420b3618f66d78ec46423392
Felipe Ribeiro <7634275@formacao.iefp.pt>
2026-09-23 11:14:16 +0100

    23/09/2026

---
+++ app/src/main/java/com/example/learnmath/AdicaoSimples.kt
@@ -0,0 +1,17 @@
+package com.example.learnmath
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AdicaoSimples : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.adicao_simples)
+
+    }
+}
---
+++ app/src/main/java/com/example/learnmath/menuAdicao.kt
@@ -0,0 +1,17 @@
+package com.example.learnmath
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class menuAdicao : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.menu_adicao)
+
+    }
+}
